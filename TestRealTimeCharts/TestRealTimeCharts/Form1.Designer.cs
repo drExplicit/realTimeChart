09a74d16b319effc7466341646ba3f5e61c9dfea
drExplicit <x3rv1th@hotmail.com>
2016-10-07 19:13:08 +0100

    Add files via upload

---
+++ TestRealTimeCharts/TestRealTimeCharts/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TestRealTimeCharts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.cpuChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cpuChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.cpuChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.cpuChart.Legends.Add(legend1);
+            this.cpuChart.Location = new System.Drawing.Point(12, 12);
+            this.cpuChart.Name = "cpuChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.cpuChart.Series.Add(series1);
+            this.cpuChart.Size = new System.Drawing.Size(882, 493);
+            this.cpuChart.TabIndex = 0;
+            this.cpuChart.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(719, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 560);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cpuChart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cpuChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart cpuChart;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TestRealTimeCharts/TestRealTimeCharts/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TestRealTimeCharts
+{
+    public partial class Form1 : Form
+    {
+        private Thread cpuThread;
+        private double[] cpuArray = new double[60];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getPerformanceCounters()
+        {
+            var cpuPerfCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+
+            while (true)
+            {
+                cpuArray[cpuArray.Length - 1] = Math.Round(cpuPerfCounter.NextValue(), 0);
+
+                Array.Copy(cpuArray, 1, cpuArray, 0, cpuArray.Length - 1);
+
+                if (cpuChart.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateCpuChart(); });
+                }
+                else
+                {
+                    //......
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void UpdateCpuChart()
+        {
+            cpuChart.Series["Series1"].Points.Clear();
+
+            for (int i = 0; i < cpuArray.Length - 1; ++i)
+            {
+                cpuChart.Series["Series1"].Points.AddY(cpuArray[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cpuThread = new Thread(new ThreadStart(this.getPerformanceCounters));
+            cpuThread.IsBackground = true;
+            cpuThread.Start();
+        }
+    }
+}
